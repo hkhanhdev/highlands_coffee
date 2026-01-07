7e31a0be8c3ad9f9ba1e4db272ed14ac7a31a0d2
hoang <nguyenxuanhakhanh88060@gmail.com>
2026-01-07 22:09:30 +0700

    Create coupons.py
    Completed source to raw pipeline

---
+++ notebooks/mappings/coupons.py
@@ -0,0 +1,132 @@
+from dask.distributed import Client
+import dask.dataframe as dd
+from pyiceberg.catalog import load_catalog
+import pandas as pd
+from pyiceberg.schema import Schema
+from pyiceberg.types import NestedField, LongType, FloatType, StringType, IntegerType,DateType,DecimalType,DoubleType
+from .modules import getDaskClient, closeDaskClient, loadIcebergCatalog, loadOrCreateIcebergTable, getSourceData,writeDaskDfToIceberg
+from prefect import get_run_logger
+
+logger = get_run_logger()
+
+TABLE = "coupons"
+TABLE_TYPE = "sm"
+TABLE_IDENTIFIER = ("raw_test", "coupons") #logical table identifier in Iceberg catalog
+TABLE_LOCATION = "s3://highlands-coffee-warehouse/raw_test/coupons" #physical location in MinIO bucket
+# Create output schema
+R_SCHEMA = Schema(
+    NestedField(1, "id", LongType(), required=False),
+    NestedField(2, "coupon_name", StringType(), required=False),
+    NestedField(3, "effective_date", StringType(), required=False),
+    NestedField(4, "discount_type", StringType(), required=False),
+    NestedField(5, "discount_value", LongType(), required=False),
+    NestedField(6, "product_usage_limit", DoubleType(), required=False),
+    NestedField(7, "usage_limit", DoubleType(), required=False),
+    NestedField(8, "effective_member_id", LongType(), required=False),
+    NestedField(9, "expiry_date", StringType(), required=False),
+)
+def s_2_r(source_path: str, source_table: str, source_date: str):
+    logger.info(f"Running {TABLE} s_2_r")
+    client = None
+    # Get Dask client
+    client_result = getDaskClient("tcp://dask-scheduler:8786")
+    if isinstance(client_result, tuple): #Handle error if any
+        return client_result
+        # raise Exception(client_result[1])
+    client = client_result #Return the client if no error
+    logger.info("✅ Successfully connected to Dask client")
+
+    # Load Iceberg catalog
+    catalog_result = loadIcebergCatalog("mycatalog")
+    if isinstance(catalog_result, tuple):
+        return catalog_result
+        # raise Exception(catalog_result[1])
+    catalog = catalog_result
+    logger.info("✅ Successfully loaded Iceberg catalog")
+
+    # Reading input data using Dask
+    df_result = getSourceData(day=source_date, table_name=TABLE, engine="dask", table_type=TABLE_TYPE)
+    if isinstance(df_result, tuple): #Handle error if any
+        return df_result
+    df = df_result
+    logger.info("✅ Successfully read source data")
+
+    # Create or load Iceberg table
+    table_result = loadOrCreateIcebergTable(
+        catalog,
+        table_identifier=TABLE_IDENTIFIER,
+        schema=R_SCHEMA,
+        location=TABLE_LOCATION,
+    )
+    if isinstance(table_result, tuple):
+        return table_result
+        # raise Exception(table_result[1])
+    table = table_result
+    logger.info("✅ Successfully loaded or created logical Iceberg table")
+
+    # Write data to MinIO using Iceberg table append
+    write_result = writeDaskDfToIceberg(df, table)
+    if isinstance(write_result, tuple):
+        return write_result
+        # raise Exception(write_result[1])
+    # arrow_table = write_result
+    # logger.info(arrow_table)
+    logger.info("✅ Successfully wrote data to Iceberg table")
+
+    close_result = closeDaskClient(client)
+    if isinstance(close_result, tuple):
+        logger.warning(f"⚠️ Error while closing Dask client: {close_result[1]}")
+    else:
+        logger.info("✅ Successfully closed Dask client")
+
+    return True, "ok"
+
+def r_2_e(source_path: str,source_table:str,source_date:str):
+    print(f"Running {TABLE} r_2_e")
+        client = None
+    # Get Dask client
+    client_result = getDaskClient("tcp://dask-scheduler:8786")
+    if isinstance(client_result, tuple): #Handle error if any
+        return client_result
+        # raise Exception(client_result[1])
+    client = client_result #Return the client if no error
+    logger.info("✅ Successfully connected to Dask client")
+
+    # Load Iceberg catalog
+    catalog_result = loadIcebergCatalog("mycatalog")
+    if isinstance(catalog_result, tuple):
+        return catalog_result
+        # raise Exception(catalog_result[1])
+    catalog = catalog_result
+    logger.info("✅ Successfully loaded Iceberg catalog")
+
+    # Reading input data using Dask
+    # df_result = getRawData(day=source_date, table_name=TABLE, engine="dask", table_type=TABLE_TYPE)
+    # if isinstance(df_result, tuple): #Handle error if any
+    #     return df_result
+    # df = df_result
+    # logger.info("✅ Successfully read raw data")
+
+    table = catalog.load_table(("raw_test", "coupons"))
+    print(table.schema())
+    df = table.scan().to_pandas()
+    print(df.head())
+    logger.info("✅ Successfully read source data")
+
+    return False, f"No action for {TABLE} r_2_e"
+
+def e_2_c(source_path: str,source_table:str,source_date:str):
+    print(f"Running {TABLE} e_2_c")
+    return False, f"No action for {TABLE} e_2_c"
+
+def main():
+
+    # catalog = load_catalog("mycatalog")
+    # catalog.drop_table(("raw_test", "coupons"))
+    # # table = catalog.load_table(("raw_test", "coupons"))
+    # # print(table.schema())
+
+    print("Coupons module executed")
+
+if __name__ == "__main__":
+    main()
